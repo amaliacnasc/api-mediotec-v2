aeeaac4705b47536a46942e5f6768710c98e6cac
amaliacnasc <amaliacnasc@gmail.com>
2024-10-07 22:24:44 -0300

    m

--- routes/notificationRoute.js
+++ routes/notificationRoute.js
@@ -5,8 +5,8 @@ const notifications = require('../controllers/notificationController');
 router.post('/', notifications.createNotification); 
 router.get('/', notifications.getAllNotifications);
 router.get('/notification/:title', notifications.getNotificationByTitle);
-router.get('/notification/id/announcementId', notifications.getNotificationById);
-router.put('/notification/announcementId', notifications.updateNotificationById); 
-router.delete('/notification/announcementId', notifications.deleteNotificationById); 
+router.get('/notification/id/:announcementId', notifications.getNotificationById);
+router.put('/notification/:announcementId', notifications.updateNotificationById); 
+router.delete('/notification/:announcementId', notifications.deleteNotificationById); 
 
 module.exports = router;
