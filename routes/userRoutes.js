6288e7cba52b468e7aeae75dbffc2123632c52e6
amaliacnasc <amaliacnasc@gmail.com>
2024-11-25 16:59:01 -0300

    alterando permissao pra buscar por email

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -6,7 +6,7 @@ const auth = require('../authMiddleware/authMiddleware');
 router.get('/', auth, User.getAllUsers); 
 router.get('/:id', auth, User.getUserById);
 router.get('/name/:name', auth, User.getUserByName); 
-router.get('/email/:email', auth, User.getUserByEmail)
+router.get('/email/:email',  User.getUserByEmail)
 router.get('/role/:role', auth, User.getUserByType); 
 router.get('/class/:classId', auth, User.getAllUsersOfClass) ; // busca todos os usuarios de uma turma 
 router.get('/course/:courseId', auth, User.getAllUsersOfCourse); // busca todos os usuarios de uma disciplina 
