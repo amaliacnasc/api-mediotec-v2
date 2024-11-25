7085736ffdb89e1ccb084b44fe1f62e8d9f547d0
amaliacnasc <amaliacnasc@gmail.com>
2024-11-25 20:12:19 -0300

    mudanca em conceitos

--- routes/conceitoRoute.js
+++ routes/conceitoRoute.js
@@ -8,7 +8,7 @@ router.post('/', conceito.createConceito );
 router.get('/conceitos', conceito.getAllConceitos); 
 router.get('/conceito/:conceitoId', conceito.getConceitoById);
 router.get('/conceitos/:courseId', conceito.getAllConceitosOfCourseId); // busca todos os conceitos de uma disciplina 
-router.get('/conceitos/user/:userId', conceito.getAllConceitosOfUserId); // busca todos os conceitos de um usuario 
+router.get('/user/:userId', conceito.getAllConceitosOfUserId); // busca todos os conceitos de um usuario 
 router.get('/conceitos/:classId/:unidade', conceito.getConceitosByClassAndUnidade);
 router.put('/conceito/update/:conceitoId', conceito.updateConceitoById); 
 router.delete('/conceito/delete/:conceitoId', conceito.deleteConceitoById);
