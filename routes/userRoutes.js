dd27ffdef652ae5c7bf757b144e074aaaa927a82
amaliacnasc <amaliacnasc@gmail.com>
2024-11-25 21:33:59 -0300

    rota usuario mudou

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,13 +4,13 @@ const User = require('../controllers/userController');
 const auth = require('../authMiddleware/authMiddleware'); 
 
 router.get('/', auth, User.getAllUsers); 
-router.get('/id/:id', User.getUserById);
+router.get('/id/:userId', User.getUserById);
 router.get('/name/:name', auth, User.getUserByName); 
 router.get('/email/:email',  User.getUserByEmail)
 router.get('/role/:role', auth, User.getUserByType); 
 router.get('/class/:classId', auth, User.getAllUsersOfClass) ; // busca todos os usuarios de uma turma 
 router.get('/course/:courseId', auth, User.getAllUsersOfCourse); // busca todos os usuarios de uma disciplina 
-router.put('/update/:id', auth, User.updateUserById); 
-router.delete('/delete/:id', auth, User.deleteUserById); 
+router.put('/update/:userId', auth, User.updateUserById); 
+router.delete('/delete/:userId', auth, User.deleteUserById); 
 
 module.exports = router;
