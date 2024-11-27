ebec27dee555830eceeab5c90695f4d6560fc6de
Rafael <rafaelmoura7033@gmail.com>
2024-11-27 16:08:10 -0300

    Removendo a rota getRelationshipById temporariamente

--- routes/relationship.js
+++ routes/relationship.js
@@ -5,7 +5,7 @@ const relationship = require('../controllers/userClassCourse');
 router.post('/', relationship.createRelationship);
 router.get('/user/:userId', relationship.getUserRelations);
 router.get('/', relationship.getAllRelationships);
-router.get('/:id', relationship.getRelationshipById);
+// router.get('/:id', relationship.getRelationshipById);
 router.put('/:id', relationship.updateRelationship);
 router.get('/users', relationship.getRelationshipsGroupedByUser);
 router.get('/courses', relationship.getRelationshipsGroupedByCourse);
