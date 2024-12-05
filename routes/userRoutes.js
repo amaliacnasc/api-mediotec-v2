a6875e1af0b51044ac0aa95c53198e1ec19340ea
Rafael Moura <87615082+RafaelMoura11@users.noreply.github.com>
2024-12-05 18:07:49 -0300

    Desabilitando o middleware auth

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,18 +3,18 @@ const router = express.Router();
 const User = require('../controllers/userController'); 
 const auth = require('../authMiddleware/authMiddleware'); 
 
-router.get('/', auth, User.getAllUsers); 
+router.get('/', User.getAllUsers); 
 router.get('/id/:userId', User.getUserById);
-router.get('/name/:name', auth, User.getUserByName); 
+router.get('/name/:name', User.getUserByName); 
 router.get('/email/:email',  User.getUserByEmail)
-router.get('/role/:role', auth, User.getUserByType); 
-router.get('/class/:classId', auth, User.getAllUsersOfClass) ; // busca todos os usuarios de uma turma 
-router.get('/course/:courseId', auth, User.getAllUsersOfCourse); // busca todos os usuarios de uma disciplina 
-router.put('/update/:userId', auth, User.updateUserById); 
-router.delete('/delete/:userId', auth, User.deleteUserById);
-router.get('/student/:userId/courses', auth, User.getAllCoursesOfStudent); // Busca disciplinas de um aluno
-router.get('/student/:userId/course/:courseId/concepts', auth, User.getConceptsByCourseAndStudent); // Busca conceitos de uma disciplina para um aluno
-router.put('/concepts/:conceitoId', auth, User.updateConcept);
+router.get('/role/:role', User.getUserByType); 
+router.get('/class/:classId', User.getAllUsersOfClass) ; // busca todos os usuarios de uma turma 
+router.get('/course/:courseId', User.getAllUsersOfCourse); // busca todos os usuarios de uma disciplina 
+router.put('/update/:userId', User.updateUserById); 
+router.delete('/delete/:userId', User.deleteUserById);
+router.get('/student/:userId/courses', User.getAllCoursesOfStudent); // Busca disciplinas de um aluno
+router.get('/student/:userId/course/:courseId/concepts', User.getConceptsByCourseAndStudent); // Busca conceitos de uma disciplina para um aluno
+router.put('/concepts/:conceitoId', User.updateConcept);
 
 
 module.exports = router;
